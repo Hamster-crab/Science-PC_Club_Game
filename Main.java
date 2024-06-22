88b5aaffaff403e3799d63760f90edba6a33196e
Hamster-Crab <hhidekitano@gmail.com>
2024-06-22 20:20:07 +0900

    一旦2

---
+++ Main.java
@@ -0,0 +1,51 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.imageio.ImageIO;
+
+public class Main {
+    public class ImageDisplayExample extends JFrame {
+        private Image img;
+
+        public ImageDisplayExample() {
+            try {
+                img = ImageIO.read(new File("Sato's/title/TitleSato.jpg"))
+            }   catch (IOException e)   {
+                e.printStackTrace();
+            }
+            setLayout(new FlowLayout());
+            setSize(256, 144);
+            set
+        }
+    }
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("愛を知らせに来た佐藤");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 500);
+        frame.setLocationRelativeTo(null);
+
+        // テキストを表示するラベルの作成
+        JLabel label1 = new JLabel("愛を知らせに来た佐藤");
+        label1.setBounds(243, 20, 350, 35);
+        label1.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        label1.setHorizontalAlignment(SwingConstants.CENTER); // 中央寄せを追加
+        panel.add(label1);
+
+        // ボタンを作成
+        JButton button1 = new JButton("すたーと");
+        button1.setBounds(233, 130, 400, 50);
+        panel.add(button1);
+
+        JButton button2 = new JButton("せってい");
+        button2.setBounds(233, 230, 400, 50);
+        panel.add(button2);
+
+        JButton button3 = new JButton("おわり");
+        button3.setBounds(233, 340, 400, 50);
+        panel.add(button3);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
