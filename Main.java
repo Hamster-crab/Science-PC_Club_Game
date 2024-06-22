2274b87bf5e4c20116465c4d74e4f09676a12f83
Hamster-Crab <hhidekitano@gmail.com>
2024-06-22 20:48:39 +0900

    タイトル画面背景以外まぁまぁ出来た

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,24 +8,27 @@ public class Main {
         frame.setSize(800, 500);
         frame.setLocationRelativeTo(null);
 
+        JPanel panel = new JPanel();
+        panel.setLayout(null);
+
         // テキストを表示するラベルの作成
         JLabel label1 = new JLabel("愛を知らせに来た佐藤");
-        label1.setBounds(243, 20, 350, 35);
-        label1.setFont(new Font("SansSerif", Font.PLAIN, 30));
+        label1.setBounds(70, 20, 650, 65);
+        label1.setFont(new Font("SansSerif", Font.PLAIN, 60));
         label1.setHorizontalAlignment(SwingConstants.CENTER); // 中央寄せを追加
         panel.add(label1);
 
         // ボタンを作成
         JButton button1 = new JButton("すたーと");
-        button1.setBounds(233, 130, 400, 50);
+        button1.setBounds(193, 130, 400, 50);
         panel.add(button1);
 
         JButton button2 = new JButton("せってい");
-        button2.setBounds(233, 230, 400, 50);
+        button2.setBounds(193, 230, 400, 50);
         panel.add(button2);
 
         JButton button3 = new JButton("おわり");
-        button3.setBounds(233, 340, 400, 50);
+        button3.setBounds(193, 340, 400, 50);
         panel.add(button3);
 
         frame.add(panel);
