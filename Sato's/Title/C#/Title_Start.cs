5dc4abdb0ebd3f88f6145f61d6ebbc2ed7a2d858
Hamster-Crab <hhidekitano@gmail.com>
2024-06-22 16:56:33 +0900

    Javaに変更 わかったな2

---
+++ Sato's/Title/C#/Title_Start.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title_Start()
+{
+    public void change_button()
+    {
+        SceneManager.LoadScene("after");
+    }
+
+}
+
