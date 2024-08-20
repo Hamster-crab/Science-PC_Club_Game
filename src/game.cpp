04c70685738b11e363841f0c79d8e45383864eac
Hamster-Crab <hhidekitano@gmail.com>
2024-08-20 18:31:56 +0900

    ワールドのマップカキカキしてる

--- src/game.cpp
+++ src/game.cpp
@@ -2,9 +2,10 @@
 
 int main(void)
 {
-    int playerX, playerY = 0;
     int screenWidth = 900;
     int screenHeight = 600;
+    int backgroundX, backgroundY = 0;
+    int playerSpeed = 8;
 
     // ウィンドウの初期化
     InitWindow(screenWidth, screenHeight, "raylib - Background Image Example");
@@ -30,32 +31,35 @@ int main(void)
     // メインループ
     while (!WindowShouldClose())
     {
-
         // プレイヤーの位置を更新
-        if (IsKeyDown(KEY_D)) playerPosition.x += 3.0f;
-        if (IsKeyDown(KEY_A)) playerPosition.x -= 3.0f;
-        if (IsKeyDown(KEY_W)) playerPosition.y -= 3.0f;
-        if (IsKeyDown(KEY_S)) playerPosition.y += 3.0f;
+        if (IsKeyDown(KEY_D)) playerPosition.x += playerSpeed;
+        if (IsKeyDown(KEY_A)) playerPosition.x -= playerSpeed;
+        if (IsKeyDown(KEY_W)) playerPosition.y -= playerSpeed;
+        if (IsKeyDown(KEY_S)) playerPosition.y += playerSpeed;
+        // プレイヤーの位置を更新
+        if (IsKeyDown(KEY_RIGHT)) playerPosition.x += playerSpeed;
+        if (IsKeyDown(KEY_LEFT)) playerPosition.x -= playerSpeed;
+        if (IsKeyDown(KEY_UP)) playerPosition.y -= playerSpeed;
+        if (IsKeyDown(KEY_DOWN)) playerPosition.y += playerSpeed;
 
         // カメラのターゲットをプレイヤーの位置に設定
         camera.target = playerPosition;
 
-        // プレイヤーが壁にあたったかどうかをチェック
-        if (playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth ||
-            playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight)
-        {
-            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-            DrawTexture(background, 500, 0, WHITE);
-            DrawTexture(background, 0, 500, WHITE);
-        }
-        // プレイヤーが壁にあたったかどうかをチェック
-        else if (playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight ||
-            playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth)
-        {
-            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-            DrawTexture(background, 0, playerPosition.y + 500, WHITE);
-            DrawTexture(background,  playerPosition.x + 500, 0, WHITE);
-        }
+//        // プレイヤーが壁にあたったかどうかをチェック
+//        if (playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth || playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight)
+//        {
+//            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
+//            DrawTexture(background, backgroundX + 500, 0, WHITE);
+//            DrawTexture(background, 0, 500, WHITE);
+//        }
+//        // プレイヤーが壁にあたったかどうかをチェック
+//        else if (playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight ||
+//            playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth)
+//        {
+//            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
+//            DrawTexture(background, 0, backgroundX + 500, WHITE);
+//            DrawTexture(background,  backgroundX + 500, 0, WHITE);
+//        }
 
             // 描画の開始
             BeginDrawing();
@@ -67,7 +71,25 @@ int main(void)
         BeginMode2D(camera);
 
         // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-        DrawTexture(background, 0, 0, WHITE);
+        DrawTexture(background, backgroundX, backgroundY, WHITE);
+
+//        横
+        // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
+
+//      左
+        DrawTexture(background, backgroundX + 500, backgroundY, WHITE);
+        DrawTexture(background, backgroundX + ( 500 / 2 ), backgroundY, WHITE);
+
+//      下
+        DrawTexture(background, backgroundX, backgroundY + 500, WHITE);
+
+//      右
+        DrawTexture(background, backgroundX - 500, backgroundY, WHITE);
+
+//        上
+        DrawTexture(background, backgroundX, backgroundY - 500, WHITE);
+
+
 
         // プレイヤーを描画
         DrawTexture(playerTexture, playerPosition.x, playerPosition.y, WHITE);
