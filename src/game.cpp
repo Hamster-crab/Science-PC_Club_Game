43503b32890807c875f83742d1b92ae3bd259c0c
Hamster-Crab <hhidekitano@gmail.com>
2024-08-21 11:05:09 +0900

    ハートを20中5追加

--- src/game.cpp
+++ src/game.cpp
@@ -6,6 +6,8 @@ int main(void)
     int screenHeight = 600;
     int backgroundX, backgroundY = 0;
     int playerSpeed = 8;
+    int playerHeartsMax = 20;
+    int playerHearts = 20;
 
     // ウィンドウの初期化
     InitWindow(screenWidth, screenHeight, "raylib - Background Image Example");
@@ -14,6 +16,14 @@ int main(void)
     Texture2D background = LoadTexture("resources/Textures/OverWorld/maptile_grasslands_one.png");  // 背景画像のパス
 
     Texture2D playerTexture = LoadTexture("resources/Textures/Beef.png");  // プレイヤー画像のパス
+
+    Image playerHeartsImageImage = LoadImage("resources/Textures/playerHeart.png");
+
+    // 画像サイズを変更
+    ImageResize(&playerHeartsImageImage, 30, 30);
+
+    // テクスチャに変換
+    Texture2D playerHeartsImage = LoadTextureFromImage(playerHeartsImageImage);
 
     // プレイヤーの初期設定
     Vector2 playerPosition = { screenWidth / 2.0f - playerTexture.width / 2.0f, screenHeight / 2.0f - playerTexture.height / 2.0f };
@@ -45,22 +55,6 @@ int main(void)
         // カメラのターゲットをプレイヤーの位置に設定
         camera.target = playerPosition;
 
-//        // プレイヤーが壁にあたったかどうかをチェック
-//        if (playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth || playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight)
-//        {
-//            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-//            DrawTexture(background, backgroundX + 500, 0, WHITE);
-//            DrawTexture(background, 0, 500, WHITE);
-//        }
-//        // プレイヤーが壁にあたったかどうかをチェック
-//        else if (playerPosition.y < 0 || playerPosition.y + playerTexture.height > screenHeight ||
-//            playerPosition.x < 0 || playerPosition.x + playerTexture.width > screenWidth)
-//        {
-//            // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-//            DrawTexture(background, 0, backgroundX + 500, WHITE);
-//            DrawTexture(background,  backgroundX + 500, 0, WHITE);
-//        }
-
             // 描画の開始
             BeginDrawing();
 
@@ -81,18 +75,47 @@ int main(void)
         DrawTexture(background, backgroundX + ( 500 / 2 ), backgroundY, WHITE);
 
 //      下
-        DrawTexture(background, backgroundX, backgroundY + 500, WHITE);
+        DrawTexture(background, backgroundX, backgroundY + ( 500 / 2 ), WHITE);
 
 //      右
-        DrawTexture(background, backgroundX - 500, backgroundY, WHITE);
+        DrawTexture(background, backgroundX - ( 500 * 2 ), backgroundY, WHITE);
 
 //        上
-        DrawTexture(background, backgroundX, backgroundY - 500, WHITE);
+        DrawTexture(background, backgroundX, backgroundY - ( 500 * 2 ), WHITE);
 
 
 
         // プレイヤーを描画
         DrawTexture(playerTexture, playerPosition.x, playerPosition.y, WHITE);
+
+        int playerHeartsPositionXOne = playerPosition.x - 440;
+        int playerHeartsPositionXTwo = playerPosition.x - 406;
+        int playerHeartsPositionXThree = playerPosition.x - 372;
+        int playerHeartsPositionXFour = playerPosition.x - 338;
+        int playerHeartsPositionXFive = playerPosition.x - 304;
+//        int playerHeartsPositionXSix = playerPosition.x - 530;
+//        int playerHeartsPositionXSeven = playerPosition.x - 550;
+//        int playerHeartsPositionXEight = playerPosition.x - 570;
+//        int playerHeartsPositionXNine = playerPosition.x - 590;
+//        int playerHeartsPositionXTen = playerPosition.x - 610;
+//        int playerHeartsPositionXEleven = playerPosition.x - 430;
+//        int playerHeartsPositionXTwelve = playerPosition.x - 430;
+//        int playerHeartsPositionXThirteen = playerPosition.x - 430;
+//        int playerHeartsPositionXFourteen = playerPosition.x - 430;
+//        int playerHeartsPositionXFifteen = playerPosition.x - 430;
+//        int playerHeartsPositionXSixteen = playerPosition.x - 430;
+//        int playerHeartsPositionXSeventeen = playerPosition.x - 430;
+//        int playerHeartsPositionXEighteen = playerPosition.x - 430;
+//        int playerHeartsPositionXNineteen = playerPosition.x - 430;
+//        int playerHeartsPositionXTwenty = playerPosition.x - 430;
+        int playerHeartsPositionY = playerPosition.y + 250;
+
+        // ♡
+        DrawTexture(playerHeartsImage, playerHeartsPositionXOne, playerHeartsPositionY, WHITE);
+        DrawTexture(playerHeartsImage, playerHeartsPositionXTwo, playerHeartsPositionY, WHITE);
+        DrawTexture(playerHeartsImage, playerHeartsPositionXThree, playerHeartsPositionY, WHITE);
+        DrawTexture(playerHeartsImage, playerHeartsPositionXFour, playerHeartsPositionY, WHITE);
+        DrawTexture(playerHeartsImage, playerHeartsPositionXFive, playerHeartsPositionY, WHITE);
 
         // カメラの描画終了
         EndMode2D();
