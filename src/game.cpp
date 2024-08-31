a8f7b27b7f68eacac85e5efd941616a961dcb0a6
Hamster-crab <hhidekitano@gmail.com>
2024-08-31 10:19:53 +0900

    のどか脇バー作った

--- src/game.cpp
+++ src/game.cpp
@@ -10,6 +10,7 @@ int main(void)
     int backgroundX = 375;
     int backgroundY = -63;
     int playerSpeed = 8;
+    int playerDryThroat = 20;
     double playerHeartsMax = 40.0;
     double playerHeartsDefault = 20.0;
     double playerHearts = 20.0;
@@ -22,11 +23,12 @@ int main(void)
     SetTargetFPS(60);
 
     // 背景画像のロード
-    Texture2D background = LoadTexture("resources/Textures/maptile_grasslands_one.png");  // 背景画像のパス
-
+    Image backgroundImage = LoadImage("resources/Nomal-World/GameMap.png");  // 背景画像のパス
     Image playerTextureTexture = LoadImage("resources/Textures/player/playerImage.png");
     Image playerFullStomachImageImage = LoadImage("resources/Textures/Beef.png");
-    Image playerWaterFullImageImage = LoadImage("resources/Textures/Water-full.png");
+    Image playerWaterFullImageImage = LoadImage("resources/Textures/Water-Full.png");
+    Image playerWaterHalfImageImage = LoadImage("resources/Textures/Water-Half.png");
+    Image playerWaterEmptyImageImage = LoadImage("resources/Textures/Water-Empty.png");
 
     int playerWidth = 150;
     int playerHeight = 200;
@@ -34,11 +36,17 @@ int main(void)
     ImageResize(&playerTextureTexture, playerWidth, playerHeight);
     ImageResize(&playerFullStomachImageImage, 30, 30);
     ImageResize(&playerWaterFullImageImage, 30, 30);
+    ImageResize(&playerWaterHalfImageImage, 30, 30);
+    ImageResize(&playerWaterEmptyImageImage, 30, 30);
+    ImageResize(&backgroundImage, 16050, 16050);
 
     // テクスチャに変換
     Texture2D playerTexture = LoadTextureFromImage(playerTextureTexture);
     Texture2D playerFullStomachImage = LoadTextureFromImage(playerFullStomachImageImage);
     Texture2D playerWaterFullImage = LoadTextureFromImage(playerWaterFullImageImage);
+    Texture2D playerWaterHalfImage = LoadTextureFromImage(playerWaterHalfImageImage);
+    Texture2D playerWaterEmptyImage = LoadTextureFromImage(playerWaterEmptyImageImage);
+    Texture2D background = LoadTextureFromImage(backgroundImage);
 
 
     // プレイヤーの初期設定
@@ -105,27 +113,7 @@ int main(void)
         BeginMode2D(camera);
 
         // 画像を描画（画面全体に表示するため、画像のサイズをウィンドウサイズに合わせる）
-        DrawTexture(background, backgroundX, backgroundY, WHITE); // 中央
-//        DrawTexture(background, backgroundX + 500, backgroundY, WHITE); // 右
-//        DrawTexture(background, backgroundX, backgroundY + 500, WHITE); // 下
-        DrawTexture(background, backgroundX - 500, backgroundY, WHITE); // 左
-        DrawTexture(background, backgroundX - 1000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 1500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 2000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 2500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 3000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 3500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 4000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 4500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 5000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 5500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 6000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 6500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 7000, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 7500, backgroundY, WHITE);
-        DrawTexture(background, backgroundX - 8000, backgroundY, WHITE);
-
-//        DrawTexture(background, backgroundX, backgroundY -500, WHITE); // 上
+        DrawTexture(background, -5000, -5000, WHITE); // 中央
 
         // プレイヤーを描画
         DrawTexture(playerTexture, playerPosition.x -100, playerPosition.y - 100, WHITE);
@@ -149,7 +137,7 @@ int main(void)
         int playerHeartsPositionYOne = playerPosition.y + 260;
         int playerHeartsPositionYTwo = playerPosition.y + 210;
         int playerFullStomachPositionY = playerPosition.y + 260;
-        int playerWaterFullPositionY = playerPosition.y + 480;
+        int playerWaterFullPositionY = playerPosition.y + 220;
 
         // ハート
 
@@ -168,15 +156,8 @@ int main(void)
         playerXY << "X : " << playerPosition.x << "\n\n" << "Y : " << playerPosition.y;
         std::string playerXYText = playerXY.str();
 
-        // 初風呂表示
-        std::ostringstream hatuBro;
-        hatuBro << "Block X : " << backgroundX - 500 << "\n\nBlock Y : " << backgroundY;
-        std::string hatuBroText = hatuBro.str();
-
         DrawText(playerHeartsText.c_str(), playerHeartsPositionXOne, playerHeartsPositionYOne, 20, BLACK);  // 変数の内容を表示
         DrawText(playerXYText.c_str(), playerHeartsPositionXOne, playerHeartsPositionYOne - 550, 20, BLACK);  // playerのxy表示
-
-        DrawText(hatuBroText.c_str(), playerHeartsPositionXOne, playerHeartsPositionYOne - 400, 20, BLACK);  // 初ブロ
 
         DrawRectangle(playerHeartsPositionXFour, playerHeartsPositionYOne, playerHeartsDefault * 12, 20, RED);
         DrawRectangle(playerHeartsPositionXFour, playerHeartsPositionYOne, playerHearts * 12, 20, YELLOW);
@@ -197,19 +178,20 @@ int main(void)
         DrawTexture(playerFullStomachImage, playerFullStomachPositionXNine, playerFullStomachPositionY, WHITE);
         DrawTexture(playerFullStomachImage, playerFullStomachPositionXTen, playerFullStomachPositionY, WHITE);
 
-        // 水ゲージ(満タン)
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXOne, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXTwo, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXThree, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXFour, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXFive, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXSix, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXSeven, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXEight, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXNine, playerWaterFullPositionY, WHITE);
-        DrawTexture(playerWaterFullImage, playerFullStomachPositionXTen, playerWaterFullPositionY, WHITE);
-
-
+        // 水ゲージ
+        if (playerDryThroat == 20)
+        {
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXOne, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXTwo, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXThree, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXFour, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXFive, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXSix, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXSeven, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXEight, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXNine, playerWaterFullPositionY, WHITE);
+            DrawTexture(playerWaterFullImage, playerFullStomachPositionXTen, playerWaterFullPositionY, WHITE);
+        }
 
         // カメラの描画終了
         EndMode2D();
